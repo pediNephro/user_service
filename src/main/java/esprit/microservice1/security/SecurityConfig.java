78360c92e7b8b5a070c8dc29c78830b7faa3e6b4
AzizTurki07 <150946968+AzizTurki07@users.noreply.github.com>
2026-05-05 19:12:36 +0000

    fix security config and user controller

--- src/main/java/esprit/microservice1/security/SecurityConfig.java
+++ src/main/java/esprit/microservice1/security/SecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
